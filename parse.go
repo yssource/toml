4ab4b86fd38fb8d1aca6419cdf941e329a864055
someone else <martin@arp242.net>
2021-06-27 14:07:41 +0800

    Positions are now mostly correct
    Still a few suboptimal ones:
    
    	––– string-bad-escape.toml –––––––––––––––––––––––––––––––––––––––––––
    	toml: error: invalid escape character 'a'; only the following escape characters are allowed: \b, \t, \n, \f, \r, \", \\, \uXXXX, and \UXXXXXXXX
    		at line 1; start 18; length 24
    		last key parsed was "invalid-escape"
    
    	1 | invalid-escape = "This string has a bad \a escape character."
    				^^^^^^^^^^^^^^^^^^^^^^^^
    
    	––– string-bad-multiline.toml ––––––––––––––––––––––––––––––––––––––––
    	toml: error: strings cannot contain newlines
    		at line 1; start 7; length 0
    		last key parsed was "multi"
    
    	1 | multi = "first line
    			^
    
    	––– string-literal-multiline-quotes-1.toml –––––––––––––––––––––––––––
    	toml: error: unexpected "''''''"
    		at line 1; start 7; length 20
    		last key parsed was "a"
    
    	1 | a = '''6 apostrophes: ''''''
    			^^^^^^^^^^^^^^^^^^^^
    
    	––– string-wrong-close.toml ––––––––––––––––––––––––––––––––––––––––––
    	toml: error: strings cannot contain newlines
    		at line 1; start 18; length 0
    		last key parsed was "bad-ending-quote"
    
    	1 | bad-ending-quote = "double and single'
    				^

--- parse.go
+++ parse.go
@@ -18,8 +18,7 @@ type parser struct {
 	ordered    []Key           // List of keys in the order that they appear in the TOML data.
 	context    Key             // Full key for the current hash in scope.
 	currentKey string          // Base key name for everything except hashes.
-	approxLine int             // Rough approximation of line number
-	pos        int             // Rough approximation of position (byte offset from start).
+	pos        Position        // Position
 	implicits  map[string]bool // Record implied keys (e.g. 'key.group.names').
 }
 
@@ -54,9 +53,8 @@ func parse(data string) (p *parser, err error) {
 	}
 	if i := strings.IndexRune(data[:ex], 0); i > -1 {
 		return nil, ParseError{
-			Message: "files cannot contain NULL bytes; probably using UTF-16; TOML files must be UTF-8",
-			Pos:     i,
-			Line:    1,
+			Message:  "files cannot contain NULL bytes; probably using UTF-16; TOML files must be UTF-8",
+			Position: Position{Line: 1},
 		}
 	}
 
@@ -78,12 +76,12 @@ func parse(data string) (p *parser, err error) {
 	return p, nil
 }
 
-func (p *parser) panicf(format string, v ...interface{}) {
+func (p *parser) panicf(it item, format string, v ...interface{}) {
 	panic(ParseError{
 		Message: fmt.Sprintf(format, v...),
-		Line:    p.approxLine,
-		Pos:     p.pos,
-		LastKey: p.current(),
+		//Position: p.pos,
+		Position: it.pos,
+		LastKey:  p.current(),
 	})
 }
 
@@ -92,7 +90,7 @@ func (p *parser) next() item {
 	//fmt.Printf("ITEM %-18s line %-3d │ %q\n", it.typ, it.line, it.val)
 	if it.typ == itemError {
 		p.pos = it.pos
-		p.panicf("%s", it.val)
+		p.panicf(it, "%s", it.val)
 	}
 	return it
 }
@@ -116,11 +114,11 @@ func (p *parser) assertEqual(expected, got itemType) {
 func (p *parser) topLevel(item item) {
 	switch item.typ {
 	case itemCommentStart: // # ..
-		p.approxLine, p.pos = item.line, item.pos
+		p.pos = item.pos
 		p.expect(itemText)
 	case itemTableStart: // [ .. ]
 		name := p.next()
-		p.approxLine, p.pos = name.line, name.pos
+		p.pos = name.pos
 
 		var key Key
 		for ; name.typ != itemTableEnd && name.typ != itemEOF; name = p.next() {
@@ -133,7 +131,7 @@ func (p *parser) topLevel(item item) {
 		p.ordered = append(p.ordered, key)
 	case itemArrayTableStart: // [[ .. ]]
 		name := p.next()
-		p.approxLine, p.pos = name.line, name.pos
+		p.pos = name.pos
 
 		var key Key
 		for ; name.typ != itemArrayTableEnd && name.typ != itemEOF; name = p.next() {
@@ -148,7 +146,7 @@ func (p *parser) topLevel(item item) {
 		outerContext := p.context
 		/// Read all the key parts (e.g. 'a' and 'b' in 'a.b')
 		k := p.next()
-		p.approxLine, p.pos = k.line, k.pos
+		p.pos = k.pos
 		var key Key
 		for ; k.typ != itemKeyEnd && k.typ != itemEOF; k = p.next() {
 			key = append(key, p.keyString(k))
@@ -203,9 +201,9 @@ var datetimeRepl = strings.NewReplacer(
 func (p *parser) value(it item, parentIsArray bool) (interface{}, tomlType) {
 	switch it.typ {
 	case itemString:
-		return p.replaceEscapes(it.val), p.typeOfPrimitive(it)
+		return p.replaceEscapes(it, it.val), p.typeOfPrimitive(it)
 	case itemMultilineString:
-		return p.replaceEscapes(stripFirstNewline(stripEscapedNewlines(it.val))), p.typeOfPrimitive(it)
+		return p.replaceEscapes(it, stripFirstNewline(stripEscapedNewlines(it.val))), p.typeOfPrimitive(it)
 	case itemRawString:
 		return it.val, p.typeOfPrimitive(it)
 	case itemRawMultilineString:
@@ -237,10 +235,10 @@ func (p *parser) value(it item, parentIsArray bool) (interface{}, tomlType) {
 
 func (p *parser) valueInteger(it item) (interface{}, tomlType) {
 	if !numUnderscoresOK(it.val) {
-		p.panicf("Invalid integer %q: underscores must be surrounded by digits", it.val)
+		p.panicf(it, "Invalid integer %q: underscores must be surrounded by digits", it.val)
 	}
 	if numHasLeadingZero(it.val) {
-		p.panicf("Invalid integer %q: cannot have leading zeroes", it.val)
+		p.panicf(it, "Invalid integer %q: cannot have leading zeroes", it.val)
 	}
 
 	num, err := strconv.ParseInt(it.val, 0, 64)
@@ -251,7 +249,7 @@ func (p *parser) valueInteger(it item) (interface{}, tomlType) {
 		// So mark the former as a bug but the latter as a legitimate user
 		// error.
 		if e, ok := err.(*strconv.NumError); ok && e.Err == strconv.ErrRange {
-			p.panicf("Integer '%s' is out of the range of 64-bit signed integers.", it.val)
+			p.panicf(it, "Integer '%s' is out of the range of 64-bit signed integers.", it.val)
 		} else {
 			p.bug("Expected integer value, but got '%s'.", it.val)
 		}
@@ -269,18 +267,18 @@ func (p *parser) valueFloat(it item) (interface{}, tomlType) {
 	})
 	for _, part := range parts {
 		if !numUnderscoresOK(part) {
-			p.panicf("Invalid float %q: underscores must be surrounded by digits", it.val)
+			p.panicf(it, "Invalid float %q: underscores must be surrounded by digits", it.val)
 		}
 	}
 	if len(parts) > 0 && numHasLeadingZero(parts[0]) {
-		p.panicf("Invalid float %q: cannot have leading zeroes", it.val)
+		p.panicf(it, "Invalid float %q: cannot have leading zeroes", it.val)
 	}
 	if !numPeriodsOK(it.val) {
 		// As a special case, numbers like '123.' or '1.e2',
 		// which are valid as far as Go/strconv are concerned,
 		// must be rejected because TOML says that a fractional
 		// part consists of '.' followed by 1+ digits.
-		p.panicf("Invalid float %q: '.' must be followed by one or more digits", it.val)
+		p.panicf(it, "Invalid float %q: '.' must be followed by one or more digits", it.val)
 	}
 	val := strings.Replace(it.val, "_", "", -1)
 	if val == "+nan" || val == "-nan" { // Go doesn't support this, but TOML spec does.
@@ -289,9 +287,9 @@ func (p *parser) valueFloat(it item) (interface{}, tomlType) {
 	num, err := strconv.ParseFloat(val, 64)
 	if err != nil {
 		if e, ok := err.(*strconv.NumError); ok && e.Err == strconv.ErrRange {
-			p.panicf("Float '%s' is out of the range of 64-bit IEEE-754 floating-point numbers.", it.val)
+			p.panicf(it, "Float '%s' is out of the range of 64-bit IEEE-754 floating-point numbers.", it.val)
 		} else {
-			p.panicf("Invalid float value: %q", it.val)
+			p.panicf(it, "Invalid float value: %q", it.val)
 		}
 	}
 	return num, p.typeOfPrimitive(it)
@@ -322,7 +320,8 @@ func (p *parser) valueDatetime(it item) (interface{}, tomlType) {
 		}
 	}
 	if !ok {
-		p.panicf("Invalid TOML Datetime: %q.", it.val)
+		p.pos = it.pos
+		p.panicf(it, "Invalid TOML Datetime: %q.", it.val)
 	}
 	return t, p.typeOfPrimitive(it)
 }
@@ -371,7 +370,7 @@ func (p *parser) valueInlineTable(it item, parentIsArray bool) (interface{}, tom
 
 		/// Read all key parts.
 		k := p.next()
-		p.approxLine, p.pos = k.line, k.pos
+		p.pos = k.pos
 		var key Key
 		for ; k.typ != itemKeyEnd && k.typ != itemEOF; k = p.next() {
 			key = append(key, p.keyString(k))
@@ -482,7 +481,8 @@ func (p *parser) addContext(key Key, array bool) {
 		case map[string]interface{}:
 			hashContext = t
 		default:
-			p.panicf("Key '%s' was already created as a hash.", keyContext)
+			it := item{pos: p.pos} // TODO
+			p.panicf(it, "Key '%s' was already created as a hash.", keyContext)
 		}
 	}
 
@@ -500,7 +500,8 @@ func (p *parser) addContext(key Key, array bool) {
 		if hash, ok := hashContext[k].([]map[string]interface{}); ok {
 			hashContext[k] = append(hash, make(map[string]interface{}))
 		} else {
-			p.panicf("Key '%s' was already created and cannot be used as an array.", keyContext)
+			it := item{pos: p.pos} // TODO
+			p.panicf(it, "Key '%s' was already created and cannot be used as an array.", keyContext)
 		}
 	} else {
 		p.setValue(key[len(key)-1], make(map[string]interface{}))
@@ -537,7 +538,8 @@ func (p *parser) setValue(key string, value interface{}) {
 		case map[string]interface{}:
 			hash = t
 		default:
-			p.panicf("Key '%s' has already been defined.", keyContext)
+			it := item{pos: p.pos} // TODO
+			p.panicf(it, "Key '%s' has already been defined.", keyContext)
 		}
 	}
 	keyContext = append(keyContext, key)
@@ -564,7 +566,8 @@ func (p *parser) setValue(key string, value interface{}) {
 
 		// Otherwise, we have a concrete key trying to override a previous
 		// key, which is *always* wrong.
-		p.panicf("Key '%s' has already been defined.", keyContext)
+		it := item{pos: p.pos} // TODO
+		p.panicf(it, "Key '%s' has already been defined.", keyContext)
 	}
 
 	hash[key] = value
@@ -659,7 +662,7 @@ func stripEscapedNewlines(s string) string {
 	return strings.Join(split, "")
 }
 
-func (p *parser) replaceEscapes(str string) string {
+func (p *parser) replaceEscapes(it item, str string) string {
 	var replaced []rune
 	s := []byte(str)
 	r := 0
@@ -680,7 +683,7 @@ func (p *parser) replaceEscapes(str string) string {
 			p.bug("Expected valid escape code after \\, but got %q.", s[r])
 			return ""
 		case ' ', '\t':
-			p.panicf("invalid escape: '\\%c'", s[r])
+			p.panicf(it, "invalid escape: '\\%c'", s[r])
 			return ""
 		case 'b':
 			replaced = append(replaced, rune(0x0008))
@@ -707,14 +710,14 @@ func (p *parser) replaceEscapes(str string) string {
 			// At this point, we know we have a Unicode escape of the form
 			// `uXXXX` at [r, r+5). (Because the lexer guarantees this
 			// for us.)
-			escaped := p.asciiEscapeToUnicode(s[r+1 : r+5])
+			escaped := p.asciiEscapeToUnicode(it, s[r+1:r+5])
 			replaced = append(replaced, escaped)
 			r += 5
 		case 'U':
 			// At this point, we know we have a Unicode escape of the form
 			// `uXXXX` at [r, r+9). (Because the lexer guarantees this
 			// for us.)
-			escaped := p.asciiEscapeToUnicode(s[r+1 : r+9])
+			escaped := p.asciiEscapeToUnicode(it, s[r+1:r+9])
 			replaced = append(replaced, escaped)
 			r += 9
 		}
@@ -722,7 +725,7 @@ func (p *parser) replaceEscapes(str string) string {
 	return string(replaced)
 }
 
-func (p *parser) asciiEscapeToUnicode(bs []byte) rune {
+func (p *parser) asciiEscapeToUnicode(it item, bs []byte) rune {
 	s := string(bs)
 	hex, err := strconv.ParseUint(strings.ToLower(s), 16, 32)
 	if err != nil {
@@ -730,7 +733,7 @@ func (p *parser) asciiEscapeToUnicode(bs []byte) rune {
 			"lexer claims it's OK: %s", s, err)
 	}
 	if !utf8.ValidRune(rune(hex)) {
-		p.panicf("Escaped character '\\u%s' is not valid UTF-8.", s)
+		p.panicf(it, "Escaped character '\\u%s' is not valid UTF-8.", s)
 	}
 	return rune(hex)
 }
